e6f4c7e03d771ff899176ba3dfc8c3148ad5459c
Harder&Harder <67688139+Allen-HuangYufan@users.noreply.github.com>
2021-05-03 17:17:09 +0800

    Add files via upload

---
+++ homework9/homework9/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework9
+{
+    public partial class Form1 : Form
+    {
+        BindingSource bdsResult = new BindingSource();
+        Crawler crawler = new Crawler();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dgvResult.DataSource = bdsResult;
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+            crawler.CrawlerStopped += Crawler_CrawlerStopped;
+        }
+
+        private void Crawler_CrawlerStopped(Crawler obj)
+        {
+            lblCondition.Text = "Crawler stopped";
+        }
+
+        private void Crawler_PageDownloaded(Crawler crawler, string url, string info)
+        {
+            var pageInfo = new { Index = bdsResult.Count + 1, URL = url, Status = info };
+            bdsResult.Add(pageInfo);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bdsResult.Clear();
+            lblCondition.Text = "Crawler started";
+            crawler.StartURL = tbxUrl.Text;
+            Match match = Regex.Match(crawler.StartURL, Crawler.urlParseRegex);
+            if (match.Length == 0) return;
+            string host = match.Groups["host"].Value;
+            crawler.HostFilter = "^" + host + "$";
+            crawler.FileFilter = "((.html?|.aspx|.jsp|.php)$|^[^.]+$)";
+            new Thread(crawler.Start).Start();//???
+        }
+    }
+}
